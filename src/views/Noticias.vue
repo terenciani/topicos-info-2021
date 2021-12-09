a8cf9ee99f023ec454585cc73bcc6f1d97c14a90
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2021-12-09 10:12:59 -0300

    Sistema de grid

--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>Notícias que irá aparecer</div>
+    <v-row>
+        <v-col cols="12" sm="6" md="4">
+            <v-card>Notícia 1</v-card>
+            
+        </v-col>
+
+        <v-col cols="12" sm="6" md="4">
+            <v-card>Notícia 2</v-card>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="4">
+            <v-card>Notícia 3</v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>

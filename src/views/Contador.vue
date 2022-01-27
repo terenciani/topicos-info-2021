048db8dce44d28f9d91e5d41983ef69f6dbd5836
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2022-01-27 16:26:50 -0300

    Incremento e decremento do contador

--- src/views/Contador.vue
+++ src/views/Contador.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>Contador</div>
+  <div>
+    <h1>Quantidade de pessoas: {{ pessoas }}</h1>
+    <v-btn fab color="pink" dark @click="pessoas = pessoas + 1">
+      <v-icon> mdi-plus </v-icon>
+    </v-btn>
+
+    <v-btn fab color="purple" dark>
+      <v-icon> mdi-minus </v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: () => ({
+    pessoas: 0,
+  }),
+};
 </script>
 
 <style>
-
 </style>
